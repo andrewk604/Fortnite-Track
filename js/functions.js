a84c0e6010d4174df7144350c0bd33df7e89aeaf
andrewk604 <30775786+andrewk604@users.noreply.github.com>
2020-12-10 10:05:13 +0100

    Main files

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('#servicesMenu');
+const services = document.querySelector('#services');
+
+function menuShow () {
+    menu.style.top = '65'+'px';
+};
+
+function menuHide () {
+    menu.style.top = '-225'+'px';
+};
